24e98a1ce8475144d715bbaa4975a5fc382286c8
John-Robert Scholz <jscholz@bgeo01m014.local>
2018-03-12 08:17:56 +0100

    uncomment import secrets.

--- hebel.py
+++ hebel.py
@@ -25,7 +25,7 @@ import datetime as dt
 import warnings
 warnings.filterwarnings("ignore", category=DeprecationWarning)
 
-from secrets.util import gmail_auth							# private libaries
+#from secrets.util import gmail_auth								# private libaries
 from email.mime.multipart import MIMEMultipart
 from email.utils import formatdate
 
@@ -37,7 +37,7 @@ import pandas_datareader.data as web
 import pandas as pd
 import requests
 import csv
-
+from apscheduler.schedulers.background import BackgroundScheduler
 
 
 ######################  functions and classes  ######################
@@ -214,7 +214,50 @@ def get_google_finance_intraday(ticker, period=60, days=1):
 	    stuff = pd.DataFrame(rows, index=pd.DatetimeIndex(times, name='Date'))
 	
 	print(stuff)
+def get_google_finance_intraday(ticker, period=60, days=1):
+    """
+    Retrieve intraday stock data from Google Finance.
 
+    Parameters
+    ----------
+    ticker : str
+        Company ticker symbol.
+    period : int
+        Interval between stock values in seconds.
+    days : int
+        Number of days of data to retrieve.
+
+    Returns
+    -------
+    df : pandas.DataFrame
+        DataFrame containing the opening price, high price, low price,
+        closing price, and volume. The index contains the times associated with
+        the retrieved price values.
+    """
+
+    uri = 'https://finance.google.com/finance/getprices' \
+          '?i={period}&p={days}d&f=d,o,h,l,c,v&df=cpct&q={ticker}'.format(ticker=ticker,
+                                                                          period=period,
+                                                                          days=days)
+    page = requests.get(uri)
+    print(uri)
+    reader = csv.reader(page.content.splitlines())
+    columns = ['Open', 'High', 'Low', 'Close', 'Volume']
+    rows = []
+    times = []
+    for row in reader:
+        if re.match('^[a\d]', row[0]):
+            if row[0].startswith('a'):
+                start = datetime.datetime.fromtimestamp(int(row[0][1:]))
+                times.append(start)
+            else:
+                times.append(start+datetime.timedelta(seconds=period*int(row[0])))
+            rows.append(map(float, row[1:]))
+    if len(rows):
+        return pd.DataFrame(rows, index=pd.DatetimeIndex(times, name='Date'),
+                            columns=columns)
+    else:
+        return pd.DataFrame(rows, index=pd.DatetimeIndex(times, name='Date'))
 
 ###  e-mail  ###
 def validMail(mail):
@@ -407,6 +450,20 @@ def main():
 	"""
 	Main program.
 	"""
+
+	sched = BackgroundScheduler()
+
+	def job_function():
+		print("Hello World")
+		time.sleep(5)
+
+
+	print(dt.datetime.now())
+
+	# Schedules job_function to be run on the third Friday
+	# of June, July, August, November and December at 00:00, 01:00, 02:00 and 03:00
+	sched.add_job(job_function, 'cron', second=1)
+	sched.start()
 
 ######################  _ _ N A M E _ _ = = " _ _ M A I N _ _ "  ##############
 if __name__ == "__main__":
